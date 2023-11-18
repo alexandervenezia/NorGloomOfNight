a239153268f8f5ad15634131081443c8a7e45ecd
PerplexerVT <harrisonnelsonperry@gmail.com>
2023-11-18 10:47:36 +0000

    Data: Updated Kerogs combat data and added cards

--- res/scripts/controllers/overworld/Enemy.cs
+++ res/scripts/controllers/overworld/Enemy.cs
@@ -10,63 +10,63 @@ using System.Linq;
 
 public partial class Enemy : CharacterBody2D, ICombatable
 {
-    [Export] protected string _name;
-    [Export] protected int _id; // Must match UID of combat equivalent
-    [Export] protected float _speed;
-    [Export] protected float _acceleration;
+	[Export] protected string _name;
+	[Export] protected int _id; // Must match UID of combat equivalent
+	[Export] protected float _speed;
+	[Export] protected float _acceleration;
 
-    protected AnimatedSprite2D _sprite;
-    protected Vector2 _spawnPoint;
+	protected AnimatedSprite2D _sprite;
+	protected Vector2 _spawnPoint;
 
-    private bool _enabled;
-    
+	private bool _enabled;
+	
 
-    public override void _Ready()
-    {
-        _enabled = true;
-        _spawnPoint = Position;
-        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    }
+	public override void _Ready()
+	{
+		_enabled = true;
+		_spawnPoint = Position;
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
 
-    public override void _Process(double delta)
-    {        
-        MoveAndSlide();
+	public override void _Process(double delta)
+	{        
+		MoveAndSlide();
 
-        for (int i = 0; i < GetSlideCollisionCount(); i++)
-        {
-            KinematicCollision2D collision = GetSlideCollision(i);
-            OnCollision(collision);
-        }
-    }
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			KinematicCollision2D collision = GetSlideCollision(i);
+			OnCollision(collision);
+		}
+	}
 
-    protected virtual void OnCollision(KinematicCollision2D collision)
-    {
+	protected virtual void OnCollision(KinematicCollision2D collision)
+	{
 
-    }
+	}
 
-    public List<int> GetEnemyIDs()
-    {
-        return new List<int>{_id};
-    }
+	public List<int> GetEnemyIDs()
+	{
+		return new List<int>{_id};
+	}
 
-    public void Disable()
-    {
-        _enabled = false;
-    }
+	public void Disable()
+	{
+		_enabled = false;
+	}
 
-    public void Enable()
-    {
-        _enabled = true;
-    }
+	public void Enable()
+	{
+		_enabled = true;
+	}
 
-    public bool IsEnabled()
-    {
-        return _enabled;
-    }
+	public bool IsEnabled()
+	{
+		return _enabled;
+	}
 
-    public void Die()
-    {
-        QueueFree();
-    }
+	public void Die()
+	{
+		QueueFree();
+	}
 
 }
