44d916c9a50bc8baa4fb57ef41e348cc6eb4b522
PerplexerVT <harrisonnelsonperry@gmail.com>
2023-11-26 16:19:13 +0000

    Whiteboxed castle layout

--- res/scripts/controllers/overworld/HealArea.cs
+++ res/scripts/controllers/overworld/HealArea.cs
@@ -5,32 +5,32 @@ using System;
 
 public partial class HealArea : Area2D
 {
-    private ILevel _level;
-    private bool _playerInArea;
+	private ILevel _level;
+	private bool _playerInArea;
 
-    public override void _Ready()
-    {
-        _level = MasterScene.GetInstance().GetActiveScene<ILevel>();
-    }
+	public override void _Ready()
+	{
+		_level = MasterScene.GetInstance().GetActiveScene<ILevel>();
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (_playerInArea)
-        {
-            if (Input.IsActionJustPressed("ui_interact"))
-            {
-                _level.GetPlayer().FullHeal();
-            }
-        }
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_playerInArea)
+		{
+			if (Input.IsActionJustPressed("ui_interact"))
+			{
+				_level.GetPlayer().FullHeal();
+			}
+		}
+	}
 
-    public void Enter()
-    {
-        _playerInArea = true;
-    }
+	public void Enter()
+	{
+		_playerInArea = true;
+	}
 
-    public void Exit()
-    {
-        _playerInArea = false;
-    }
+	public void Exit()
+	{
+		_playerInArea = false;
+	}
 }
