920706a325288e628201eeea1a0d382e02e00bfe
PerplexerVT <harrisonnelsonperry@gmail.com>
2023-11-30 00:14:33 +0000

    NPC stuff

--- res/scripts/controllers/overworld/NPC.cs
+++ res/scripts/controllers/overworld/NPC.cs
@@ -7,74 +7,74 @@ using System.Threading.Tasks;
 
 public partial class NPC : Area2D
 {
-    [Export] private Godot.Collections.Array<string> _dialogue;
-    [Export] private int _delayMS;
-    private List<string> _dialogueLines;
-    private ILevel _level;
-    private Polygon2D _dialogueBubble;
-    private RichTextLabel _dialogueBubbleText;
-    private bool _playerInArea;
-    private bool _dialogueRunning;
+	[Export] private Godot.Collections.Array<string> _dialogue;
+	[Export] private int _delayMS;
+	private List<string> _dialogueLines;
+	private ILevel _level;
+	private Polygon2D _dialogueBubble;
+	private RichTextLabel _dialogueBubbleText;
+	private bool _playerInArea;
+	private bool _dialogueRunning;
 
-    public override void _Ready()
-    {
-        _level = MasterScene.GetInstance().GetActiveScene<ILevel>();
-        _dialogueBubble = GetNode<Polygon2D>("DialogueBubble");
-        _dialogueBubbleText = _dialogueBubble.GetNode<RichTextLabel>("RichTextLabel");
+	public override void _Ready()
+	{
+		_level = MasterScene.GetInstance().GetActiveScene<ILevel>();
+		_dialogueBubble = GetNode<Polygon2D>("DialogueBubble");
+		_dialogueBubbleText = _dialogueBubble.GetNode<RichTextLabel>("RichTextLabel");
 
-        _dialogueLines = new();
+		_dialogueLines = new();
 
-        foreach (string l in _dialogue)
-        {
-            if (l.Length == 0)
-                continue;
-            _dialogueLines.Add(l);
-        }
+		foreach (string l in _dialogue)
+		{
+			if (l.Length == 0)
+				continue;
+			_dialogueLines.Add(l);
+		}
 
-        AreaEntered += OnAreaEntered;
-        AreaExited += OnAreaExited;
-    }
+		AreaEntered += OnAreaEntered;
+		AreaExited += OnAreaExited;
+	}
 
-    public override void _Process(double delta)
-    {
-        if (_playerInArea && Input.IsActionJustPressed("ui_interact") && !_dialogueRunning)
-        {
-            GD.Print("Dialogue started");
-            StartDialogue();
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (_playerInArea && Input.IsActionJustPressed("ui_interact") && !_dialogueRunning)
+		{
+			GD.Print("Dialogue started");
+			StartDialogue();
+		}
+	}
 
-    public void OnAreaEntered(Area2D area)
-    {
-        if (area.GetParent() is Player)
-        {
-            GD.Print("Entered");
-            _playerInArea = true;
-        }
-    }
+	public void OnAreaEntered(Area2D area)
+	{
+		if (area.GetParent() is Player)
+		{
+			GD.Print("Entered");
+			_playerInArea = true;
+		}
+	}
 
-    public void OnAreaExited(Area2D area)
-    {
-        if (area.GetParent() is Player)
-        {
-            GD.Print("Exited");
-            _playerInArea = false;
-        }
-    }
+	public void OnAreaExited(Area2D area)
+	{
+		if (area.GetParent() is Player)
+		{
+			GD.Print("Exited");
+			_playerInArea = false;
+		}
+	}
 
-    private async void StartDialogue()
-    {        
-        _dialogueRunning = true;
+	private async void StartDialogue()
+	{        
+		_dialogueRunning = true;
 
-        foreach (string line in _dialogueLines)
-        {
-            _dialogueBubbleText.Text = line;
-            await Task.Delay(_delayMS);
-        }
+		foreach (string line in _dialogueLines)
+		{
+			_dialogueBubbleText.Text = line;
+			await Task.Delay(_delayMS);
+		}
 
-        _dialogueRunning = false;
-        _dialogueBubbleText.Text = "[center]\n\n. . .";
-    }
+		_dialogueRunning = false;
+		_dialogueBubbleText.Text = "[center]\n\n. . .";
+	}
 
 
 }
