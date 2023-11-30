92d9f94abe37773c20b6fd9a8cdfdbeed941047f
PerplexerVT <harrisonnelsonperry@gmail.com>
2023-11-30 12:20:34 +0000

    data: shop

--- res/scripts/controllers/overworld/Door/DoorPair.cs
+++ res/scripts/controllers/overworld/Door/DoorPair.cs
@@ -6,52 +6,52 @@ using System.Numerics;
 [Tool]
 public partial class DoorPair : Node2D
 {
-    [Export] private Vector2I _doorOnePos;
-    [Export] private Vector2I _doorTwoPos;
+	[Export] private Vector2I _doorOnePos;
+	[Export] private Vector2I _doorTwoPos;
 
-    [Export] private bool _showDoorOne;
-    [Export] private bool _showDoorTwo;
+	[Export] private bool _showDoorOne;
+	[Export] private bool _showDoorTwo;
 
-    private Sprite2D _doorOne;
-    private Sprite2D _doorTwo;
+	private Sprite2D _doorOne;
+	private Sprite2D _doorTwo;
 
-    public override void _Ready()
-    {
-        if (!Engine.IsEditorHint())
-        {
-            Node[] nodes = GetChildren().ToArray<Node>();
-            _doorOne = (Door)nodes[0];
-            _doorTwo = (Door)nodes[1];
-            _doorOne.Position = _doorOnePos;
-            _doorTwo.Position = _doorTwoPos;
-            ((Door)(_doorOne)).SetPair((Door)_doorTwo);
-            ((Door)(_doorTwo)).SetPair((Door)_doorOne);
+	public override void _Ready()
+	{
+		if (!Engine.IsEditorHint())
+		{
+			Node[] nodes = GetChildren().ToArray<Node>();
+			_doorOne = (Door)nodes[0];
+			_doorTwo = (Door)nodes[1];
+			_doorOne.Position = _doorOnePos;
+			_doorTwo.Position = _doorTwoPos;
+			((Door)(_doorOne)).SetPair((Door)_doorTwo);
+			((Door)(_doorTwo)).SetPair((Door)_doorOne);
 
-            _doorOne.Visible = _showDoorOne;
-            _doorOne.Visible = _showDoorTwo;
-        }
-    }
+			_doorOne.Visible = _showDoorOne;
+			_doorOne.Visible = _showDoorTwo;
+		}
+	}
 
-    public override void _Process(double delta)
-    {
-        if (Engine.IsEditorHint())
-        {
-            if (_doorOne == null || _doorTwo == null)
-            {
-                Node[] nodes = GetChildren().ToArray<Node>();
-                _doorOne = (Sprite2D)nodes[0];
-                _doorTwo = (Sprite2D)nodes[1];
-            }
-            else
-            {
-                _doorOne.Position = _doorOnePos;
-                _doorTwo.Position = _doorTwoPos;
-            }
-            
-        }
-        else
-        {
-            
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (Engine.IsEditorHint())
+		{
+			if (_doorOne == null || _doorTwo == null)
+			{
+				Node[] nodes = GetChildren().ToArray<Node>();
+				_doorOne = (Sprite2D)nodes[0];
+				_doorTwo = (Sprite2D)nodes[1];
+			}
+			else
+			{
+				_doorOne.Position = _doorOnePos;
+				_doorTwo.Position = _doorTwoPos;
+			}
+			
+		}
+		else
+		{
+			
+		}
+	}
 }
